e079836500694086b3f03873ffa8e11a0fc204b8
mapcrafter2048 <121042282+mapcrafter2048@users.noreply.github.com>
2023-11-09 00:58:53 +0530

    modal changes

--- src/pages/myclass.js
+++ src/pages/myclass.js
@@ -138,15 +138,15 @@ export default function Myclass(){
               &#8203;
             </span>
             <div
-              className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+              className="inline-block align-bottom rounded-lg text-left overflow-hidden transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full custom-box-shadow"
               role="dialog"
               aria-modal="true"
               aria-labelledby="modal-headline"
             >
               <form onSubmit={handleSubmit}>
-                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="bg-gray-300 px-4 pt-5 pb-4 sm:p-6 sm:pb-4 ">
                   <h2
-                    className="text-lg leading-6 font-medium text-gray-900"
+                    className="text-xl leading-6 font-semibold text-gray-900 mb-6"
                     id="modal-headline"
                   >
                     Add Class
@@ -163,13 +163,13 @@ export default function Myclass(){
                         type="text"
                         name="batch"
                         id="batch"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        className="mt-2 mb-4 focus:ring-indigo-500 focus:border-indigo-500 block w-full h-8 shadow-sm sm:text-sm border-gray-300 rounded-md"
                         value={newClass.batch}
                         onChange={handleInputChange}
                         required
                       />
                     </div>
-                    <div className="mt-4">
+                    <div className="mt-2">
                       <label
                         className="block text-sm font-medium text-gray-700"
                         htmlFor="course"
@@ -180,13 +180,13 @@ export default function Myclass(){
                         type="text"
                         name="course"
                         id="course"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        className="mt-2 mb-4 focus:ring-indigo-500 focus:border-indigo-500 block w-full h-8 shadow-sm sm:text-sm border-gray-300 rounded-md"
                         value={newClass.course}
                         onChange={handleInputChange}
                         required
                       />
                     </div>
-                    <div className="mt-4">
+                    <div className="mt-2">
                       <label
                         className="block text-sm font-medium text-gray-700"
                         htmlFor="strength"
@@ -197,7 +197,7 @@ export default function Myclass(){
                         type="number"
                         name="strength"
                         id="strength"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        className="mt-2 mb-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full h-8 shadow-sm sm:text-sm border-gray-300 rounded-md"
                         value={newClass.strength}
                         onChange={handleInputChange}
                         required
@@ -205,17 +205,17 @@ export default function Myclass(){
                     </div>
                   </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <div className="bg-gray-300 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                   <button
                     type="submit"
-                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-500 text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
                   >
                     Create
                   </button>
                   <button
                     type="button"
                     onClick={closeModal}
-                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-50 sm:mt-0 sm:w-auto sm:text-sm"
                   >
                     Cancel
                   </button>
